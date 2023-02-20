45dca5056ff1209c15b06d4ddb796e9015f5696e
T&R <bottest9@gmail.com>
2023-02-19 19:46:09 -0600

    rescaling tools
    added some tools for rescaling datasets to smaller more manageable resolutions.

--- colorstudies.py
+++ colorstudies.py
@@ -8,11 +8,18 @@
 #--------
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.image as mpimg
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from PIL import Image, ImageOps
 import math
 import os
 from pathlib import Path
 import time
+import pandas as pd
+import cv2
+import extcolors
+from colormap import rgb2hex
 
 #----------
 # FUNCTIONS
@@ -45,6 +52,25 @@ def avgColor(path):
     img.close()
     return avg1
 
+#Single Color Average
+def singleColorAverage(listOfFiles):
+    #create an empty array to store the average color of each individual image
+    allAvgs = np.empty([0, 3])
+    #keeps track of index for placement and reporting progress
+    index = 0
+    #iterate over the images
+    for f in listOfFiles:
+        print("Processing: {i}/{t}".format(i=(index+1), t=len(listOfFiles)))
+        #get the average color of the image
+        imavg = avgColor(f)
+        imavg2 = np.array([imavg]) #turn it into the right shape
+        allAvgs = np.concatenate((allAvgs, imavg2), axis=0) #concatenate
+        #incriment the array
+        index = index + 1
+    #return the average color of the iterated images
+    #print(allAvgs)
+    print("The average Color of these images is: {}".format(np.rint(np.average(allAvgs, axis=0))))
+
 #average colors of the image along 
 def avgColorH(path):
     img = Image.open(path)
@@ -57,7 +83,34 @@ def avgColorH(path):
     img.close()
     return avg0
 
-#count the #of times each color value showes up in an image
+#Get the Average color relative to verticle position on the image
+def HeightAverageColor(listOfFiles):
+    #get the height of the image
+    img = Image.open(listOfFiles[0])
+    height = np.asarray(img).shape[0]
+    img.close()
+    #create an empty array to store the average color of each individual image's height
+    allAvgs = np.empty([0, height, 3])
+    index = 0
+    #iterate over the images
+    for f in listOfFiles:
+        print("Processing: {i}/{t}".format(i=(index+1), t=len(listOfFiles)))
+        #get the average color of the image
+        imavg = avgColorH(f)
+        imavg2 = np.array([imavg]) #turn it into the right shape
+        allAvgs = np.concatenate((allAvgs, imavg2), axis=0) #concatenate
+        #incriment the array
+        index = index + 1
+    #return the average color of the iterated images
+    #print(allAvgs)
+    print("Average Color per height generated! Created output image in working directory.")
+    havg = np.rint(np.average(allAvgs, axis=0)).astype(np.uint8) #make the values all neat and get the average
+    havgImg = np.tile(havg, ((havg.shape[0] // 10), 1, 1)) #create an array of image size which we can turn into a picture
+    out = Image.fromarray(havgImg)
+    #determine where to save
+    out.save("Average Color by Height.jpg")
+
+#count the # of times each color value showes up in an image
 #slow, might not even be what I need
 #takes about 33 seconds for 1 12MP image :/ :/
 def colorCount(path):
@@ -75,58 +128,48 @@ def colorCount(path):
     print("Image processed in {:f} seconds".format(time.time() - start))
     return tbr
 
+#rezie an image to something smaller and save it
+#reduce image to % size
+def shrinkImg(path, percent):
+    img = Image.open(path)
+    #jsut to make sure we have the input formatted right
+    if(percent >= 1.0):
+        percent = percent/100
+    #calculate the new image size
+    nwidth = int(percent * img.size[0])
+    nheight = int(percent * img.size[1])
+    #apply it
+    img = img.resize((nwidth, nheight), Image.HAMMING)
+    #save the image
+    #done this way due to how I personally am storing the images
+    #/source/images from a day/[images]
+    #and saves them to
+    #/source/resized/smol[resized images]
+    fname = "\\smol_" + str(path).split('\\')[-1]
+    #might need to convert path to (path(path.etc))
+    saveloc = str(Path(path).parent.parent.absolute()) + "\\smol_master" + fname
+    img.save(saveloc)
+
+#prepares a given folder to be data processed by shrinking all files and saving them to a new location
+#shrinks all images to a given % size
+def shrinkSet(listOfFiles, percent):
+    index = 0
+    #iterate over the images
+    for file in listOfFiles:
+        print("Processing: {i}/{t}".format(i=(index+1), t=len(listOfFiles))) #console logging for fun
+        shrinkImg(file, percent) #actually shrink the images
+        index += 1
+    print("All Images shrunk to {p}% size; Saved to: smol_master.".format(p=percent))
+
 #-----------------------
 # CODE THAT DOES STUFF!!
 #-----------------------
-#get absolute paths of images we want to study
-test = getImages()
-#print(test)
-#colorCount(input("image path: "))
-
-'''#Get the Average color relative to verticle position on the image
-#get the height of the image
-img = Image.open(test[0])
-height = np.asarray(img).shape[0]
-img.close()
-#create an empty array to store the average color of each individual image's height
-allAvgs = np.empty([0, height, 3])
-index = 0
-#iterate over the images
-for f in test:
-    print("Processing: {i}/{t}".format(i=(index+1), t=len(test)))
-    #get the average color of the image
-    imavg = avgColorH(f)
-    imavg2 = np.array([imavg]) #turn it into the right shape
-    allAvgs = np.concatenate((allAvgs, imavg2), axis=0) #concatenate
-    #incriment the array
-    index = index + 1
-#return the average color of the iterated images
-#print(allAvgs)
-print("Average Color per height generated! Created output image in working directory.")
-havg = np.rint(np.average(allAvgs, axis=0)).astype(np.uint8) #make the values all neat and get the average
-havgImg = np.tile(havg, ((havg.shape[0] // 10), 1, 1)) #create an array of image size which we can turn into a picture
-out = Image.fromarray(havgImg)
-#determine where to save
-out.save("Average Color by Height.jpg")
-'''
-
-'''#Single Color Average
-#create an empty array to store the average color of each individual image
-allAvgs = np.empty([0, 3])
-#keeps track of index for placement and reporting progress
-index = 0
-#iterate over the images
-for f in test:
-    print("Processing: {i}/{t}".format(i=(index+1), t=len(test)))
-    #get the average color of the image
-    imavg = avgColor(f)
-    imavg2 = np.array([imavg]) #turn it into the right shape
-    allAvgs = np.concatenate((allAvgs, imavg2), axis=0) #concatenate
-    #incriment the array
-    index = index + 1
-#return the average color of the iterated images
-#print(allAvgs)
-print("The average Color of these images is: {}".format(np.rint(np.average(allAvgs, axis=0))))
-'''
-#print(test)
-#avgColor(input("Path to Image: "))
+if __name__ == "__main__":
+    #get absolute paths of images we want to study
+    test = getImages()
+    #print(test)
+    #process the images
+    shrinkSet(test, .25)
+    #colorCount(input("image path: "))
+    #print(test)
+    #avgColor(input("Path to Image: "))
